eadabf26d5a2fe19982359478970589b43405804
Chonlasak Tumna <137064083+ChonlasakThamana@users.noreply.github.com>
2023-08-16 21:14:33 +0700

    7-4

--- Homework/HW07/hw07-4-prime-number.cpp
+++ Homework/HW07/hw07-4-prime-number.cpp
@@ -14,3 +14,37 @@
     Output:
         97 89 83 79 73 71 67 61 59 53 47 43 41 37 31 29 23 19 17 13 11 7 5 3 2
 */
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int userInput;
+    
+    printf("Enter number: ");
+    scanf("%d", &userInput);
+    
+    int currentNumber = userInput - 1;
+    
+    printf("Output:\n");
+    do {
+        if (isPrime(currentNumber)) {
+            printf("%d ", currentNumber);
+        }
+        currentNumber--;
+    } while (currentNumber >= 2);
+    
+    printf("\n");
+    
+    return 0;
+}
